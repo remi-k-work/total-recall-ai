0f36f2405168d5ca03b99bdc9bf971cb77ac5bfb
Remi <remi.k.work@gmail.com>
2026-04-23 20:33:17 +0200

    a

--- src/components/hero/Logo.tsx
+++
@@ -1,9 +0,0 @@
-// next
-import Image from "next/image";
-
-// assets
-import logo from "@/assets/logo.jpg";
-
-export default function Logo() {
-  return <Image src={logo} alt="Total Recall AI" loading="eager" className="h-auto w-full object-contain" />;
-}
